9d6cb71de54479eadc29cd3f319539fbdaaebab4
Taniachakraborty2000 <152197031+Taniachakraborty2000@users.noreply.github.com>
2023-12-26 17:04:49 +0530

    Add files via upload

---
+++ Factorial of an integer using while loop.py
@@ -0,0 +1,7 @@
+num=int(input("enter any number"))#4
+fact=1
+i=1
+while(i<=num):#1<=4,2<=4,3<=4,4<=4,5<=4
+    fact=fact*i#1*1,1*2,2*3,6*4=24
+    i+=1#2,3,4,5
+print("Factorial of",num,"is",fact)#24
